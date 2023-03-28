3ce3ab8bd78860062afb73d63e3a25b52af3c903
Natalia <nataliaion25@yandex.ru>
2023-03-28 19:32:57 +0400

    week14

---
+++ index.js
@@ -0,0 +1,78 @@
+let array = [
+    {
+      name: 'Стивен Спилберг',
+      career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
+      films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE',
+      top_rated_film: 'Список Шиндлера'
+    },
+    {
+      name: 'Кристофер Нолан',
+      career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
+      films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
+      top_rated_film: 'Начало'
+    },
+    {
+      name: 'Мартин МакДона',
+      career: 'Сценарист, Режиссёр, Продюсер',
+      films: 'https://www.film.ru/person/martin-makdonah',
+      top_rated_film: 'Три билборда на границе Эббинга, Миссури'
+    },
+    {
+      name: 'Алексей Балабанов',
+      career: 'Режиссёр, Сценарист, Актёр, Продюсер',
+      films: 'https://www.film.ru/person/aleksey-balabanov',
+      top_rated_film: 'Брат'
+    },
+    {
+      name: 'Питер Фаррелли',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+      films: 'https://www.film.ru/person/piter-farrelli',
+      top_rated_film: 'Зелёная книга'
+    },
+    {
+      name: 'Юрий Быков',
+      career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
+      films: 'https://www.film.ru/person/yuriy-bykov',
+      top_rated_film: 'Дурак'
+    },
+    {
+      name: 'Жан-Марк Валле',
+      career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
+      films: 'https://www.film.ru/person/zhan-mark-valle',
+      top_rated_film: 'Далласский клуб покупателей'
+    },
+  ];
+
+
+  const directorNames = [];
+  for (const user of array) {
+      directorNames.push(user.name);
+  }
+  document.querySelectorAll('.director').forEach((n, i) => n.textContent = directorNames[i]);
+
+  const careers = [];
+  for (const careersName of array) {
+      careers.push(careersName.career);
+  }
+document.querySelectorAll('.activity').forEach((n, i) => n.textContent = careers[i]);
+
+const linkNames = [];
+  for (const userlink of array) {
+    linkNames.push(userlink.films);
+  }
+  document.querySelectorAll('.links').forEach((n, i) => n.textContent = linkNames[i]);
+
+
+  const topFilmsList = [];
+for (const filmsname of array) {
+    topFilmsList.push(filmsname.top_rated_film);
+}
+const allFilmsString = topFilmsList.join(', ');
+document.getElementById('bestFilms').textContent = allFilmsString;
+
+
+
+
+
+
+
